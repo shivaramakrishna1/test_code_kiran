703397c7a2816faed289cc0b0eeaa944a4fa7bbc
root <root@ip-172-31-36-49.us-east-2.compute.internal>
2019-02-15 13:16:04 +0000

    commited

---
+++ jenkins.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                def listfile = load "${env.WORKSPACE}//jobList.txt"
+
+          def finalList = listfile.readLines()
+        finalList.each{line ->
+        println 'list value is : ' + line
+        }
+            }
+        }
+      
+
+       
+    }
+}

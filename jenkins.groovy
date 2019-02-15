c49065a9f1327c340b639fa481826d7425bf2e9a
shivaramakrishna <36416908+shivaramakrishna1@users.noreply.github.com>
2019-02-15 18:58:20 +0530

    test
    test

--- jenkins.groovy
+++ jenkins.groovy
@@ -4,10 +4,12 @@ pipeline {
     stages {
         stage('Build') {
             steps {
+                step{
                  def listfile = load "${env.WORKSPACE}//jobList.txt"
             def finalList = listfile.readLines()
         finalList.each{line ->
         println 'list value is : ' + line }
+                }
             }
         }
         stage('Test') {
